7224b08746ad43e9053709bbaa60d574a30b58dc
uglyCode <appostnikov@gmail.com>
2019-04-12 22:43:33 +0300

    dropdown menu for login added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,61 @@
 import React from 'react';
 
-const Login = (props) => {
-    return(
-        <div className='w-25 pr3 flex items-center justify-end bg-lightest-blue br3'>
-            <div className = 'f5'>
-                <label htmlFor="INN">{"INN: "}</label>
-                <input
-                    className="tc"
-                    placeholder="INN" type="text" name="INN">
-                </input>
+class Login extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isDroppedDown: false,
+            userINN: 770011202
+        };
+    }
+
+    toggleMenu = (event)=>{
+        event.preventDefault();
+        this.setState(prevState => {
+            if (!prevState.isDroppedDown) document.addEventListener('click', this.closeMenu);
+            return {isDroppedDown: !prevState.isDroppedDown};
+        });
+    };
+
+    closeMenu = (event) => {
+        event.preventDefault();
+        this.setState({isDroppedDown: false})
+        document.removeEventListener('click', this.closeMenu);
+    };
+
+
+    render() {
+        const el = document.querySelector('#innSign');
+
+        return(
+            <div className='w-25 pr3 flex items-center justify-end bg-lightest-blue br3'>
+                { this.state.userINN ?
+                    <p id='innSign' className='pointer f5 underline-hover dark-blue'
+                       onClick={this.toggleMenu}>
+                        {this.state.userINN}
+                    </p> :
+                    <div className = 'f5'>
+                        <label htmlFor="INN">{"INN: "}</label>
+                        <input
+                            className="tc"
+                            placeholder="INN" type="text" name="INN">
+                        </input>
+                        <button className="ma1">{"Proceed"}</button>
+                    </div>
+                }
+                {this.state.isDroppedDown &&
+                    <ul id='dropdown'
+                        className="absolute pa0"
+                        style={{top: el.offsetTop, listStyleType: "none", backgroundColor:'rgba(255,255,255,0.8)'}}>
+                        <li className='ma3 pointer underline-hover'> Log out </li>
+                        <li className='ma3 pointer underline-hover'> Profile </li>
+                        <li className='ma3 pointer underline-hover'> Orders </li>
+                    </ul>
+                }
             </div>
-            <button className="ma1">{"Proceed"}</button>
-        </div>
-    )
+        )
+}
 };
 
 export default Login;
