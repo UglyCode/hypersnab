172d985adb4de76cfacdebca58309edb8c9ef023
uglyCode <appostnikov@gmail.com>
2019-04-23 21:43:37 +0300

    React state and effect hooks tested

--- src/components/BasketCard.js
+++ src/components/BasketCard.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 
 const BasketCard = (props) => {
+
+    const [sum, setSum] = useState(100);
+
+    useEffect(()=>{
+        document.title = `React hooks says: ${sum}`;
+        return ()=> alert(`${sum} : ORLY?`);
+    });
+
     return(
         <div className='w-20 items-center tc pl3 f5 br3 bg-lightest-blue'>
             <div className='flex-column ma2'>
                 <div>{'CART: '}</div>
-                <div className = ''>
-                    {'(11)3600RUR'}
+                <div className = ''
+                onClick={()=>setSum(sum+11)}>
+                    {`(11)${sum}RUR`}
                 </div>
             </div>
 
