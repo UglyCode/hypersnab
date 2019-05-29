cef15654cabbb5413bfe4c0ca547e339819cdb87
uglyCode <appostnikov@gmail.com>
2019-05-29 22:01:16 +0300

    Nav sticky bug fixed! Shadow!

--- src/containers/NavSticky.js
+++ src/containers/NavSticky.js
@@ -18,8 +18,6 @@ class NavSticky extends React.Component{
         this.setState({scroll: window.scrollY});
     };
 
-
-
     componentDidMount() {
         const el = document.querySelector('#navSticky');
         this.setState({top: el.offsetTop, height: el.offsetHeight});
@@ -32,48 +30,52 @@ class NavSticky extends React.Component{
 
     render() {
 
+        const isStick = this.state.scroll > this.state.top;
         let navClass = "flex items-stretch bw-2 shadow-5-l tc bg-transparent"
-            + (this.state.scroll > this.state.top ?
+            + (isStick ?
                 " fixed w-100 left-0 top-0 pr3 pl3" :
                 " br3 ma2");
         const pageLinkClass = "f6 f5-l link black-90 underline-hover pointer hover-blue dib pa3 ph4-l";
         const route = this.props.route;
 
         return(
-            <nav id='navSticky' className={navClass}>
-                <BasketCard/>
-                <div className='w-80 flex justify-between items-center bg-white'>
-                    <div>
-                        <a name='catalog'
-                           className={pageLinkClass + ((route==='catalog') ? ' ba' : '')}
-                           onClick={this.pageLinkOnClick}
-                        >Catalog</a>
-                        <a name='about'
-                           className={pageLinkClass + ((route==='about') ? ' ba' : '')}
-                           onClick={this.pageLinkOnClick}
-                        >About</a>
-                        <a name='contacts'
-                           className={pageLinkClass + ((route==='contacts') ? ' ba' : '')}
-                           onClick={this.pageLinkOnClick}
-                        >Contacts</a>
+            <div>
+                {isStick && <div className='w-100 bg-transparent' style={{height: this.state.height}}>{'shadow'}</div>}
+                <nav id='navSticky' className={navClass}>
+                    <BasketCard/>
+                    <div className='w-80 flex justify-between items-center bg-white'>
+                        <div>
+                            <a name='catalog'
+                               className={pageLinkClass + ((route==='catalog') ? ' ba' : '')}
+                               onClick={this.pageLinkOnClick}
+                            >Catalog</a>
+                            <a name='about'
+                               className={pageLinkClass + ((route==='about') ? ' ba' : '')}
+                               onClick={this.pageLinkOnClick}
+                            >About</a>
+                            <a name='contacts'
+                               className={pageLinkClass + ((route==='contacts') ? ' ba' : '')}
+                               onClick={this.pageLinkOnClick}
+                            >Contacts</a>
+                        </div>
+                        <div className="dib ph4-l flex justify-around">
+                            <input
+                                className="f6 f5-l w-90"
+                                placeholder="поиск товаров" type="text" name="searchText">
+                            </input>
+                            <button className='w-10'>
+                                <img src={require('../static/search.png')} className='mw-100'></img>
+                            </button>
+                        </div>
                     </div>
-                    <div className="dib ph4-l flex justify-around">
-                        <input
-                            className="f6 f5-l w-90"
-                            placeholder="поиск товаров" type="text" name="searchText">
-                        </input>
-                        <button className='w-10'>
-                            <img src={require('../static/search.png')} className='mw-100'></img>
-                        </button>
-                    </div>
-                </div>
-                <Login
-                    toggleProfile = {this.props.toggleProfile}
-                    setUserStatus = {this.props.setUserStatus}
-                    inn = {this.props.inn}
-                    userStatus={this.props.userStatus}
-                />
-            </nav>
+                    <Login
+                        toggleProfile = {this.props.toggleProfile}
+                        setUserStatus = {this.props.setUserStatus}
+                        inn = {this.props.inn}
+                        userStatus={this.props.userStatus}
+                    />
+                </nav>
+            </div>
         )
 
     }
