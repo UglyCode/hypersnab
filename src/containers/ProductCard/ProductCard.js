55c813e852cbadc11c9ebe5758420c2ae4ae6433
uglyCode <appostnikov@gmail.com>
2019-08-05 22:06:26 +0300

    bug fixed

--- src/containers/ProductCard/ProductCard.js
+++ src/containers/ProductCard/ProductCard.js
@@ -36,7 +36,7 @@ class ProductCard extends React.Component{
                         {'Закрыть карточку'}
                     </div>
                     <div style={{width: '200px'}} className='center'>
-                        <img src={(this.state.img).replace('3?50x50',`${this.state.code}?2000x200`)} className="db" alt='spec img'/>
+                        <img src={(this.state.img).replace('3?size=50x50',`${this.state.code}?size=200x200`)} className="db" alt='spec img'/>
                     </div>
                         <h2 className="f3 mt2 mb0 black-60 b">
                             {`${this.state.description} (${this.state.code})`}
