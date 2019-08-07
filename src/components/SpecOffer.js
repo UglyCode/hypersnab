f2434c6e2b21445875b66b0d6a8351fcd4a64cea
uglyCode <appostnikov@gmail.com>
2019-08-07 22:36:58 +0300

    bug fixed

--- src/components/SpecOffer.js
+++ src/components/SpecOffer.js
@@ -6,7 +6,7 @@ const SpecOffer = (props) => {
         <article className='bb b--black-60 ma2 pointer' onClick={()=>props.setSelectedItem(props.item)}>
             <a className="link w-100" href="#0">
                 <div style={{width: '150px'}} className='w-100 center'>
-                    <img src={(props.img).replace('3?size=50x50',`${props.item.code}?size=150x150`)} className="db" alt='spec img'/>
+                    <img src={(props.img).replace('3?size=50x50',`${props.item.code}?size=120x120`)} className="db" alt='spec img'/>
                 </div>
                 <div className="v-top pl2 tc">
                     <h2 className="f6 fw4 mt2 mb0 black-60 b">
