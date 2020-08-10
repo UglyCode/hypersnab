56419a28dbe9033142ff1209b047b76032da0210
uglyCode <appostnikov@gmail.com>
2020-08-10 16:35:36 +0300

    prod server added to enviroment

--- src/settings/env.js
+++ src/settings/env.js
@@ -1,6 +1,6 @@
 
 const ENV = {
-        server: (process.env.REACT_APP_PROD) ? 'https://hypersnab-server.herokuapp.com/' : 'https://thawing-tundra-53917.herokuapp.com' ,
+        server: (process.env.REACT_APP_PROD) ? 'https://hypersnab-server.herokuapp.com' : 'https://thawing-tundra-53917.herokuapp.com' ,
         imgPath: 'https://res.cloudinary.com/ddjw7nmgb/image/upload/goods/',
         imgThumbPath: 'https://res.cloudinary.com/ddjw7nmgb/image/upload/c_thumb,w_50,g_face/goods/',
         prod: process.env.REACT_APP_PROD
